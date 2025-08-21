0ab0a78f34732d32fa474ccc39e63ac39fd63a5d
JASURBE77 <animesila89@gmail.com>
2025-08-21 18:47:42 +0500

    defg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,21 @@
-    import React, { createContext, useState } from 'react'
+    import React, { useState } from 'react'
     import hamstertanga from '../assets/hamstertanga.svg'
     import hamstergaday from '../assets/hamstergaday.png'
     import { getItem } from 'localforage'
     import quvvat from '../assets/quvvat.png'
 
-    export const Hamstermoney = createContext()
     const Home = () => {
         const [count , setcount] = useState(Number(localStorage.getItem("money")) || 0)
         const database = localStorage.setItem("money" , count)
 
     return (
         <>
-      <Hamstermoney.Provider value={count}>
           <h1 className='flex items-center text-4xl justify-center mt-[30px]'><img className='w-[42px] h-[42px]' src={hamstertanga} alt="" />{count}</h1>
         <center>
             <button onClick={() => setcount(count + 1)}>
         <img className='mt-[30px] active:scale-90 transition-[300ms]'  src={hamstergaday} alt="" />
             </button>   
         </center>
-      </Hamstermoney.Provider>
         <div className='flex justify-around'>
             <div className='flex gap-2 items-center'>
                 <img src={quvvat} alt="" />
--- src/pages/Mine.jsx
+++ src/pages/Mine.jsx
@@ -1,12 +1,77 @@
-  import React, { useContext } from 'react'
-  import { Hamstermoney } from './Home'
-
+  import React, { useContext, useState } from 'react'
+    import hamstertanga from '../assets/hamstertanga.svg'
+import excluede from '../assets/Exclude.png'
+import coin from '../assets/hamstertanga.svg'
+import meme from '../assets/meme.png'
+import x10 from '../assets/x10.png'
   const Mine = () => {
-    const coin = useContext(Hamstermoney)
+    const [money , setmoney] = useState(localStorage.getItem('money'))
+    console.log(money);
+    
     return (
       <>
-      <div>
-        <h1>{coin}</h1>
+          <h1 className='flex items-center text-4xl justify-center mt-[30px]'><img className='w-[42px] h-[42px]' src={hamstertanga} alt="" />{money}</h1>
+      <div className='flex  justify-center items-center gap-5 mt-5 flex-col'>
+          <div className='w-[319px] rounded-2xl px-5 py-2 bg-[#32363C]'>
+            <div className='flex items-center border-b gap-7 mb-2  border-gray-500 pl-6 h-[106px]'>
+              <img src={excluede} alt="" />
+              <div className='flex flex-col gap-1'>
+                <p>Тоp 10 cmc pairs</p>
+                <p className='text-gray-500'>Profit per hour</p>
+                <div className='flex items-center gap2'>
+                  <img src={hamstertanga} alt="" />
+                  <p>120</p>
+                </div>
+              </div>
+            </div>
+            <div className='flex gap-10 pl-5'>
+              <p className='border-r border-gray-500 w-15'>lvl 1</p>
+              <div className='flex items-center gap-2'>
+                <img className='w-[26px]' src={hamstertanga} alt="" />
+                <p>192</p>
+              </div>
+            </div>
+          </div>
+           <div className='w-[319px] rounded-2xl px-5 py-2 bg-[#32363C]'>
+            <div className='flex items-center border-b gap-7 mb-2  border-gray-500 pl-6 h-[106px]'>
+              <img src={meme} alt="" />
+              <div className='flex flex-col gap-1'>
+                <p>Mene coins</p>
+                <p className='text-gray-500'>Profit per hour</p>
+                <div className='flex items-center gap2'>
+                  <img src={hamstertanga} alt="" />
+                  <p>376</p>
+                </div>
+              </div>
+            </div>
+            <div className='flex gap-10 pl-5'>
+              <p className='border-r border-gray-500 w-15'>lvl 1</p>
+              <div className='flex items-center gap-2'>
+                <img className='w-[26px]' src={hamstertanga} alt="" />
+                <p>2.3k</p>
+              </div>
+            </div>
+          </div>
+           <div className='w-[319px] rounded-2xl px-5 py-2 bg-[#32363C]'>
+            <div className='flex items-center border-b gap-7 mb-2  border-gray-500 pl-6 h-[106px]'>
+              <img src={x10} alt="" />
+              <div className='flex flex-col gap-1'>
+                <p>Margin trading x10</p>
+                <p className='text-gray-500'>Profit per hour</p>
+                <div className='flex items-center gap2'>
+                  <img src={hamstertanga} alt="" />
+                  <p>997</p>
+                </div>
+              </div>
+            </div>
+            <div className='flex gap-10 pl-5'>
+              <p className='border-r border-gray-500 w-15'>lvl 1</p>
+              <div className='flex items-center gap-2'>
+                <img className='w-[26px]' src={hamstertanga} alt="" />
+                <p>11.2k</p>
+              </div>
+            </div>
+          </div>
       </div>
       </>
     )
